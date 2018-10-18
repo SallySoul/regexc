84279c47b209cb527266891666764aacf9ca4144
SallySoul <SallySoul@protonmail.com>
2018-10-17 22:59:08 -0700

    Add error machinery

--- src/regex.pl
+++ src/regex.pl
@@ -1,28 +1,43 @@
-gram_expr(Ast_Node) --> gram_or(Ast_Node).
+enumeration([], []).
+enumeration(Ls, Es) :- enumerate_r(Ls, Es, 0).
+enumerate_r([], [], _).
+enumerate_r([L|Ls], [(L, C)|Es], C) :- N is C + 1, enumerate_r(Ls, Es, N).
 
-gram_or(Ast_Node) --> gram_concat(Ast_Node).
-gram_or(ast_or(Ast_Node1, Ast_Node2)) --> gram_concat(Ast_Node1), ['|'], gram_concat(Ast_Node2).
+gram_expr(Ast_Node, Errors) --> gram_or(Ast_Node, Errors).
 
-gram_concat(Ast_Node) --> gram_occurance(Ast_Node).
-gram_concat(ast_concat(Ast_Node1, Ast_Node2)) --> gram_occurance(Ast_Node1), gram_concat(Ast_Node2).
+gram_or(Ast_Node, Errors) --> gram_concat(Ast_Node, Errors).
+gram_or(ast_or(Ast_Node1, Ast_Node2), All_Errors) --> 
+  gram_concat(Ast_Node1, Errors1), 
+  ['|'], 
+  gram_concat(Ast_Node2, Errors2),
+  { append(Errors1, Errors2, All_Errors)}.
 
-gram_occurance(Ast_Node) --> gram_single(Ast_Node).
-gram_occurance(ast_occurance(Ast_Node, none, none)) --> gram_single(Ast_Node), ['*'].
-gram_occurance(ast_occurance(Ast_Node, none, 1)) --> gram_single(Ast_Node), ['?'].
-gram_occurance(ast_occurance(Ast_Node, 1, none)) --> gram_single(Ast_Node), ['+'].
+gram_concat(Ast_Node, Errors) --> gram_occurance(Ast_Node, Errors).
+gram_concat(ast_concat(Ast_Node1, Ast_Node2), All_Errors) --> 
+  gram_occurance(Ast_Node1, Errors1), 
+  gram_concat(Ast_Node2, Errors2),
+  { append(Errors1, Errors2, All_Errors)}.
 
-gram_single(ast_char(X)) --> [ X ], { char(X) }.
-gram_single(ast_wildcard) --> [ '.' ].
-gram_single(Ast_Node) --> ['('], gram_expr(Ast_Node), [')'].
+gram_occurance(Ast_Node, Errors) --> gram_single(Ast_Node, Errors).
+gram_occurance(ast_occurance(Ast_Node, none, none), Errors) --> gram_single(Ast_Node, Errors), ['*'].
+gram_occurance(ast_occurance(Ast_Node, none, 1), Errors) --> gram_single(Ast_Node, Errors), ['?'].
+gram_occurance(ast_occurance(Ast_Node, 1, none), Errors) --> gram_single(Ast_Node, Errors), ['+'].
 
-string_ast(String, Root_Node) :- 
+gram_single(ast_char(X), []) --> [ X ], { char(X) }.
+gram_single(ast_wildcard, []) --> [ '.' ].
+gram_single(Ast_Node, Errors) --> ['('], gram_expr(Ast_Node, Errors), [')'].
+gram_single(Ast_Node, All_Errors) --> 
+  ['('], 
+  gram_expr(Ast_Node, Errors),
+  {append(Errors, [error("Expected ')'")], All_Errors)}.
+
+string_ast(String, Root_Node, Errors) :- 
   % We use the atom-as-char, let swipl deal with specifics
   % So this takes string, relates it to a list of chars
   string_chars(String, Cs), 
  
   % Here we relate the list of chars to the grammer
-  phrase(gram_expr(Root_Node), Cs).
-
+  phrase(gram_expr(Root_Node, Errors), Cs).
 
 
 
