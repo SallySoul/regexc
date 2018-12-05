f9c3e9a41e30c92d972114df13fde75c0cb4c18f
SallySoul <SallySoul@protonmail.com>
2018-12-04 17:18:25 -0800

    Work on interface, but mostly to get rid of unnecessary tree.pl whihc was spewing warnings

--- src/interface.pl
+++ src/interface.pl
@@ -5,31 +5,44 @@
 regexc_app_spec(App_Spec) :-
   App_Spec = [
     [ 
-      opt(input), 
-      type(term), 
+      opt(input), type(atom), 
       shortflags([i]), longflags(['input']),
-      help(['regular expresion to compile'])
+      help('regular expresion to compile')
     ],
     [ 
-      opt(ast_dot), 
-      type(term),
+      opt(ast_dot), type(atom),
       longflags(['ast-dot']),
-      help(['Save the dot representation of the AST, to the specified path'])
+      help('Save the dot representation of the AST, to the specified path')
     ],
     [
-      opt(ast_nfa),
-      type(term),
+      opt(ast_nfa), type(atom),
       longflags(['ast-nfa']),
-      help(['Save the dot representation of the NFA to the specified path'])
+      help('Save the dot representation of the NFA to the specified path')
+    ],
+    [
+      opt(help), type(boolean), default(false),
+      shortflags([h]), longflags(['help']),
+      help('Show the usage of the tool')
     ]
   ].
+
+show_help_if_needed(Opts, App_Spec) :-
+  member(help(true), Opts), !,
+  opt_help(App_Spec, Help_Message),
+  write(user_output, Help_Message),
+  halt(1).
+
+show_help_if_needed(_, _).
 
 main(Args) :-
   regexc_app_spec(App_Spec),
   opt_parse(App_Spec, Args, Opts, Positional_Args, []),
-  opt_help(App_Spec, Help_Message),
-  write(Help_Message),
-  write(Positional_Args).
+  %opt_help(App_Spec, Help_Message),
+  %  write(Help_Message),
+  show_help_if_needed(Opts, App_Spec),
+  
+  writeln(Opts),
+  writeln(Positional_Args).
 
   
 
--- src/load.pl
+++ src/load.pl
@@ -1,5 +1,4 @@
 :- use_module('util.pl').
 :- use_module('regex.pl').
-:- use_module('tree.pl').
 :- use_module('statemachine.pl').
 
